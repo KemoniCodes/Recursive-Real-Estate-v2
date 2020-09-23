bbc9b75fa4a4d4c77b4edeb46d16d94c54f9068a
Kemoni <58960485+KemoniCodes@users.noreply.github.com>
2020-09-22 21:36:38 -0700

    user authentication and login route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User')
 
@@ -18,4 +22,69 @@ router.get('/', auth, async (req, res) => {
     }
 });
 
+// @route  POST api/auth
+// @desc   Authenticate a user & get token
+// @access Public *no token needed*
+router.post('/', [
+
+    //Login Validations
+    check('email', 'Please enter a valid email')
+        .isEmail(),
+    check('password', 'Password is required')
+        .exists()
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        //If there are errors then...
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { email, password } = req.body;
+
+    try {
+        // See if user exists
+        let user = await User.findOne({ email });
+        if (!user) {
+            // If user doesnt exist then..
+            return res.status(400).json({
+                errors:
+                    [{ msg: 'Invalid Credentials' }]
+            });
+        }
+
+
+        //Checking if password exists
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            //If password doesnt exist then..
+            return res.status(400).json({
+                errors:
+                    [{ msg: 'Invalid Credentials' }]
+            });
+        }
+
+
+        // Return jsonwebtoken
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token })
+            });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+
+});
+
 module.exports = router;
+
