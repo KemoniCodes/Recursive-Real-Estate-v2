7f40a4b763a674a7dfdd83b262b828a8942a2b11
Kemoni <58960485+KemoniCodes@users.noreply.github.com>
2020-09-27 13:21:30 -0700

    Delete Profile & User

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -116,7 +116,32 @@ router.get('/user/:user_id', async (req, res) => {
         }
         res.status(500).send('Server Error');
     }
-})
+});
+
+
+// @route    DELETE api/profile
+// @desc     Delete profile, user, and posts(properties)
+// @access   Private
+router.delete('/', auth, async (req, res) => {
+    try {
+        //@todo - remove users posts
+
+        //Remove Profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+
+        //Remove user
+        await User.findOneAndRemove({ _id: req.user.id });
+
+        res.json({ msg: 'User has been deleted.' })
+
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind == 'ObjectId') {
+            res.status(400).json({ msg: 'Profile is not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+});
 
 
 module.exports = router;
